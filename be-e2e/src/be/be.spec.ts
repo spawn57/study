aef1fbf1514b8193f669a2aa14fd543cd8f07407
Sunil Khiatani <spawn57@yahoo.com>
2023-12-17 20:08:24 +0800

    linting installed but not working yet

--- be-e2e/src/be/be.spec.ts
+++ be-e2e/src/be/be.spec.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 describe('GET /api', () => {
   it('should return a message', async () => {
     const res = await axios.get(`/api`);
-
+    const b = 1;
     expect(res.status).toBe(200);
     expect(res.data).toEqual({ message: 'Hello API' });
   });
--- be/src/app/app.service.ts
+++ be/src/app/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    const a = 1;
     return { message: 'Hello API' };
   }
 }
