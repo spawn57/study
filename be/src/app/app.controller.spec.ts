5b0817fc413ecff4c05990988771c6ad461ee349
Sunil Khiatani <spawn57@yahoo.com>
2023-12-30 15:13:13 +0800

    add login api

--- be/src/app/app.controller.spec.ts
+++ be/src/app/app.controller.spec.ts
@@ -2,21 +2,47 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppModule}  from './app.module';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
 
 describe('AppController', () => {
-  let app: TestingModule;
+  let app: INestApplication;
 
   beforeAll(async () => {
-    app = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+    // app = await Test.createTestingModule({
+    //   controllers: [AppController],
+    //   providers: [AppService],
+    // }).compile();
+
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      // .overrideProvider(appService)
+      // .useValue(appService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
   });
 
-  describe('getData', () => {
-    it('should return "Hello API"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Hello API' });
+  // describe('getData', () => {
+  //   it('should return "Hello API"', () => {
+  //     const appController = app.get<AppController>(AppController);
+  //     expect(appController.getData()).toEqual({ message: 'Hello API' });
+  //   });
+  // });
+
+  describe('login', () => {
+    it('should login sucessfully', () => {
+      return request(app.getHttpServer())
+        .post('/login')
+        .send({ username: 'test', password: 'test' })
+        .expect(200);
     });
   });
+
+  afterAll(async () => {
+    await app.close();
+   });
 });
--- be/src/app/app.service.ts
+++ be/src/app/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getData(): { message: string } {
     return { message: 'Hello API' };
   }
+
+  login(username: string, password: string): boolean {
+    return true;
+  }
 }
